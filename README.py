9619520016ffff9b3e7b1bfeb0fc177618480c6b
adissauro <adissauro.games@gmail.com>
2026-01-20 09:48:42 -0300

    Add HTML content to README.py

--- README.py
+++ README.py
@@ -1,1 +1,18 @@
-# index.html
+<!DOCTYPE html>
+<html>
+  <head>
+        <title>Informação útil</title>
+        <link rel="stylesheet" href="style.css">
+  </head>
+<body>
+    <h1>FamiRex Games!</h1>
+    <p>Isso é apenas um parágrafo do texto</p>
+    <h2>Jogos já feitos pela FamiRex!</h2>
+             <ul>
+                   <li>Duck Puzzle.</li>
+                   <li>Musical Salvation Last Beat.</li>
+             </ul>
+     <img src="https://media.discordapp.net/attachments/1233569479380828200/1445454634402713832/Capa_Oficial_Musical_Salvation_Last_Beat.png?ex=696c6448&is=696b12c8&hm=6b096032308045d14524ade0e9fcb5ce08639997eec89251ba1b711a50cdacf2&=&format=webp&quality=lossless&width=1240&height=698" alt="Image 1">
+     <iframe src="https://api.wo-cloud.com/content/widget/?geoObjectKey=3705599&language=pt&region=BR&timeFormat=HH:mm&windUnit=kmh&systemOfMeasurement=metric&temperatureUnit=celsius" name="CW2" scrolling="no" width="290" height="318" frameborder="0" style="border: 1px solid #10658E;border-radius: 8px"></iframe>
+</body>
+</html>
