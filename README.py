f5d5373ba58bc152e381aa5abac29aad8307db8f
adissauro <adissauro.games@gmail.com>
2026-02-03 15:24:45 -0300

    Convert HTML to Flask application structure

--- README.py
+++ README.py
@@ -1,18 +1,55 @@
-<!DOCTYPE html>
-<html>
-  <head>
-        <title>Informação útil</title>
-        <link rel="stylesheet" href="style.css">
-  </head>
-<body>
-    <h1>FamiRex Games!</h1>
-    <p>Isso é apenas um parágrafo do texto</p>
-    <h2>Jogos já feitos pela FamiRex!</h2>
-             <ul>
-                   <li>Duck Puzzle.</li>
-                   <li>Musical Salvation Last Beat.</li>
-             </ul>
-     <img src="https://media.discordapp.net/attachments/1233569479380828200/1445454634402713832/Capa_Oficial_Musical_Salvation_Last_Beat.png?ex=696c6448&is=696b12c8&hm=6b096032308045d14524ade0e9fcb5ce08639997eec89251ba1b711a50cdacf2&=&format=webp&quality=lossless&width=1240&height=698" alt="Image 1">
-     <iframe src="https://api.wo-cloud.com/content/widget/?geoObjectKey=3705599&language=pt&region=BR&timeFormat=HH:mm&windUnit=kmh&systemOfMeasurement=metric&temperatureUnit=celsius" name="CW2" scrolling="no" width="290" height="318" frameborder="0" style="border: 1px solid #10658E;border-radius: 8px"></iframe>
-</body>
-</html>
+# Importações
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+def result_calculate(size, lights, device):
+    """Calcula o consumo estimado com base na área, número de luminárias e aparelhos.
+
+    Args:
+        size (int): Tamanho (área) da residência
+        lights (int): Quantidade de luminárias
+        device (int): Quantidade de aparelhos
+
+    Returns:
+        float: Consumo estimado
+    """
+    # Coeficientes usados no cálculo do consumo de energia
+    home_coef = 100
+    light_coef = 0.04
+    devices_coef = 5
+    return size * home_coef + lights * light_coef + device * devices_coef
+
+# A primeira página
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# A segunda página
+@app.route('/<size>')
+def lights(size):
+    return render_template(
+                            'lights.html', 
+                            size=size
+                           )
+
+# A terceira página
+@app.route('/<size>/<lights>')
+def electronics(size, lights):
+    return render_template(
+                            'electronics.html',
+                            size = size, 
+                            lights = lights                           
+                           )
+
+# Cálculo
+@app.route('/<size>/<lights>/<device>')
+def end(size, lights, device):
+    return render_template('end.html', 
+                            result=result_calculate(int(size),
+                                                    int(lights), 
+                                                    int(device)
+                                                    )
+                        )
+app.run(debug=True)
